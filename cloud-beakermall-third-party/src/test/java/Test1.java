99c6cc55ed0631b4fdadde65aa888d048243eed3
WenS1n <865047755@qq.com>
2022-05-06 15:10:35 +0800

    隐藏

--- cloud-beakermall-third-party/src/test/java/Test1.java
+++ cloud-beakermall-third-party/src/test/java/Test1.java
@@ -29,8 +29,6 @@ public class Test1 {
         // yourEndpoint填写Bucket所在地域对应的Endpoint。以华东1（杭州）为例，Endpoint填写为https://oss-cn-hangzhou.aliyuncs.com。
 //        String endpoint = "oss-cn-chengdu.aliyuncs.com";
 //// 阿里云账号AccessKey拥有所有API的访问权限，风险很高。强烈建议您创建并使用RAM用户进行API访问或日常运维，请登录RAM控制台创建RAM用户。
-//        String accessKeyId = "LTAI5t7ioARg3RzBHwQi47KG";
-//        String accessKeySecret = "X3V9KEjCnYcH8u3ERudFpti7SHrWr6";
 //// 填写Bucket名称，例如examplebucket。
 //        String bucketName = "cloud-beakermall";
 // 填写文件名。文件名包含路径，不包含Bucket名称。例如exampledir/exampleobject.txt。
